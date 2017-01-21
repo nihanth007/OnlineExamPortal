8190df99a46fb9bb5302f8a1834c126e2ebf6977
Nihanth Charan Mutluru <nihanthmbc@outlook.com>
2017-01-22 02:05:17 +0530

    Latest

--- OnlineExamPortal/Global.asax.cs
+++ OnlineExamPortal/Global.asax.cs
@@ -14,5 +14,14 @@ namespace OnlineExamPortal
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
+
+        protected void Application_BeginRequest(Object sender, EventArgs e)
+        {
+            if (HttpContext.Current.Request.IsSecureConnection.Equals(false) && HttpContext.Current.Request.IsLocal.Equals(false))
+            {
+                Response.Redirect("https://" + Request.ServerVariables["HTTP_HOST"] + HttpContext.Current.Request.RawUrl);
+            }
+        }
+
     }
 }
